b45f645ebb917fab54355fca630c79feecea7889
hara-PG <hara.job2021@gmail.com>
2022-03-23 23:30:00 +0900

    null_nudefinedTest01

---
+++ null_undefined/test01/test01.js
@@ -0,0 +1,9 @@
+//undefined: 初期化されていない
+//null: 現在利用できない
+
+console.log(undefined == undefined); //true
+console.log(null == undefined); //true
+
+console.log(0 == undefined); //false
+console.log('' == undefined); //false
+console.log(false == undefined); //false
