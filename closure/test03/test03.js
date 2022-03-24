25622cbb86c7d5f16fb69e7e4ce0ecd037e54cec
hara-PG <hara.job2021@gmail.com>
2022-03-24 23:43:52 +0900

    closure test03

---
+++ closure/test03/test03.js
@@ -0,0 +1,17 @@
+// リヴィーディングモジュールというコーディングパターン
+
+function createCounter() {
+    let val = 0;
+    return {
+        increment() { val++ },
+        getVal() { return val }
+    }
+}
+
+let counter = createCounter();
+
+counter.increment();
+console.log(counter.getVal()); // 1
+
+counter.increment();
+console.log(counter.getVal()); // 2
