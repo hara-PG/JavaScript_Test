75919e1e70ebd45bd121a48d070a579c6e022791
hara-PG <hara.job2021@gmail.com>
2022-03-25 22:03:42 +0900

    丸め誤差

--- Number/test01/test01.js
+++ Number/test01/test01.js
@@ -12,3 +12,18 @@ console.log({max: Number.MAX_SAFE_INTEGER, min: Number.MIN_SAFE_INTEGER});
 // {max: 9007199254740991, min: -9007199254740991}
 // 数値の限界は、組み込みの数値型Number.MAX_SAFE_INTEGERとNumber.MIN_SAFE_INTEGERによって決まります。
 
+//数値における安全とは丸め誤差が起きた数値ではないことが確実である
+console.log(Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2); // true
+console.log(Number.MIN_SAFE_INTEGER - 1 === Number.MIN_SAFE_INTEGER - 2); // true
+
+console.log(Number.MAX_SAFE_INTEGER);      // 9007199254740991
+console.log(Number.MAX_SAFE_INTEGER + 1);  // 9007199254740992 - Correct
+console.log(Number.MAX_SAFE_INTEGER + 2);  // 9007199254740992 - Rounded!
+console.log(Number.MAX_SAFE_INTEGER + 3);  // 9007199254740994 - Rounded - correct by luck
+console.log(Number.MAX_SAFE_INTEGER + 4);  // 9007199254740996 - Rounded!
+
+// 安全な数値
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)); // true
+
+// 危険な値
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1)); // false
